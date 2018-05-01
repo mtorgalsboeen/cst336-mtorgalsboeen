c955db565c5427e9b3d90a07a2d560c433a08a92
Mathias Torgalsboeen <mtorgalsboeen@csumb.edu>
2018-05-01 22:27:02 +0000

    Fixed index.php in my final project

--- workspace/projects/final/index.php
+++ workspace/projects/final/index.php
@@ -4,9 +4,17 @@
         <title>CST336 - Final</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>        
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+        <link rel="stylesheet" href="css/stylesheet.css" type="text/css" />
     </head>
     <body>
-        <h1>Welcome!!</h1>
+        <h1>CST336 - Final Project</h1>
+        <div class="scrollmenu">
+          <a href="index.php">Home</a>
+          <a href="shop.php">Shop</a>
+          <a href="about.html">About</a>
+          <a href="contact.html">Contact</a>
+          <a href="login.php">Login</a>
+        </div>        
     </body>
 </html>
