15faae59f5120f90b259b3b7af0178ceff8ecd72
Mathias Torgalsboeen <mtorgalsboeen@csumb.edu>
2018-05-03 19:40:26 +0000

    Added database functionality for heroku

--- workspace/labs/lab9/connect.php
+++ workspace/labs/lab9/connect.php
@@ -8,13 +8,14 @@ function getDBConnection() {
     $username = "mtorgalsboeen";
     $password = "cst336";
     
+    //mysql://b5583204ad69fb:41a8adbb@us-cdbr-iron-east-05.cleardb.net/heroku_9be7bc2b5b932f8?reconnect=true
     //when connecting from Heroku
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $host = $url["host"];
-        $dbName = substr($url["path"], 1);
-        $username = $url["user"];
-        $password = $url["pass"];
+        $host = $url["us-cdbr-iron-east-05.cleardb.net"];
+        $dbName = substr($url["heroku_9be7bc2b5b932f8"], 1);
+        $username = $url["b5583204ad69fb"];
+        $password = $url["41a8adbb"];
     } 
     
     try {
