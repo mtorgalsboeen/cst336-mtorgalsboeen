94211eba79d9d71edb93d72d19a4f3e19ee69118
Mathias Torgalsboeen <mtorgalsboeen@csumb.edu>
2018-05-03 19:45:06 +0000

    Added database functionality for heroku

--- workspace/labs/lab9/connect.php
+++ workspace/labs/lab9/connect.php
@@ -3,19 +3,24 @@
 function getDBConnection() {
     
     //C9 db info
-    $host = "localhost";
+  /* $host = "localhost";
     $dbName = "csumb_quiz";
     $username = "mtorgalsboeen";
-    $password = "cst336";
+    $password = "cst336"; */
+    
+    $host = "us-cdbr-iron-east-05.cleardb.net";
+    $dbName = "heroku_9be7bc2b5b932f8";
+    $username = "b5583204ad69fb";
+    $password = "41a8adbb";    
     
     //mysql://b5583204ad69fb:41a8adbb@us-cdbr-iron-east-05.cleardb.net/heroku_9be7bc2b5b932f8?reconnect=true
     //when connecting from Heroku
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $host = $url["us-cdbr-iron-east-05.cleardb.net"];
-        $dbName = substr($url["heroku_9be7bc2b5b932f8"], 1);
-        $username = $url["b5583204ad69fb"];
-        $password = $url["41a8adbb"];
+        $host = $url["host"];
+        $dbName = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
     } 
     
     try {
